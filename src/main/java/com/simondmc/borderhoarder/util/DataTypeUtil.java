c25eff94f965364fa6ceea80126817c6ecc519c8
RKlazinga <rembrandt.klazinga@hotmail.com>
2026-01-11 16:51:34 +0100

    Remove duplicate and unused code

--- src/main/java/com/simondmc/borderhoarder/util/DataTypeUtil.java
+++ src/main/java/com/simondmc/borderhoarder/util/DataTypeUtil.java
@@ -4,18 +4,6 @@ import java.util.Map;
 import java.util.Objects;
 
 public class DataTypeUtil {
-
-    public static String joinStringArray(String[] array, String separator) {
-        StringBuilder sb = new StringBuilder();
-        for (int i = 0; i < array.length; i++) {
-            sb.append(array[i]);
-            if (i != array.length - 1) {
-                sb.append(separator);
-            }
-        }
-        return sb.toString();
-    }
-
     // https://stackoverflow.com/a/2904266
     public static <T, String> T getKeyByCaseInsensitiveString(Map<T, String> map, String value) {
         for (Map.Entry<T, String> entry : map.entrySet()) {
